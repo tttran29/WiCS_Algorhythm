71f0ec87ceb6f8dc97663e7bbf2aa710d12f46c4
hannguyen0712 <hannguyen0712@gmail.com>
2023-11-15 19:23:14 -0700

    Login component started

--- backend/src/main/java/com/example/demo/Controller.java
+++ backend/src/main/java/com/example/demo/Controller.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +9,6 @@ public class Controller {
     
     @GetMapping("/")
     public String hello() {
-        return "Hello World";
+        return "Hey WiCS!";
     }
 }
